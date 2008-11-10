61aaec986b13eaeded5150657de605facb23f775
Trevor Fountain <doches@gmail.com>
2008-11-10 21:34:41 +0000

    Using wordnet as a cli dictionary.

---
+++ examples/dictionary.rb
@@ -0,0 +1,24 @@
+# Use WordNet as a command-line dictionary.
+require 'rubygems'
+require 'wordnet'
+
+if ARGV.size != 1
+  puts "Usage: ruby dictionary.rb word"
+  exit(1)
+end
+
+word = ARGV[0]
+
+# Point this to your WordNet installation.
+WordNet::WordNet.path = "/home/doches/Code/WordNet/WordNet-3.0"
+
+# Find all the lemmas for a word (i.e., whether it occurs as a noun, verb, etc.)
+lemmas = WordNet::WordNet.find(word)
+
+# Print out each lemma with a list of possible meanings.
+lemmas.each do |lemma| 
+  puts lemma
+  lemma.synsets.each_with_index do |synset,i|
+    puts "\t#{i+1}) #{synset.gloss}"
+  end
+end
