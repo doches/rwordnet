321fe943cefddab0af71545719d081edeb43d0ed
Trevor Fountain <doches@gmail.com>
2015-08-05 12:39:09 +0100

    Inline documentation for synset getters

--- lib/wordnet/synset.rb
+++ lib/wordnet/synset.rb
@@ -4,7 +4,36 @@ module WordNet
   # Represents a synset (or group of synonymous words) in WordNet. Synsets are related to each other by various (and numerous!)
   # relationships, including Hypernym (x is a hypernym of y <=> x is a parent of y) and Hyponym (x is a child of y)
   class Synset
-    attr_reader :gloss, :synset_offset, :lex_filenum, :synset_type, :word_counts, :pos_offset, :pos
+    # Get the offset, in bytes, at which this synset's information is stored in WordNet's internal DB.
+    # You almost certainly don't care about this.
+    attr_reader :synset_offset
+
+    # A two digit decimal integer representing the name of the lexicographer file containing the synset for the sense.
+    # Probably only of interest if you're using a wordnet database marked up with custom attributes, and you
+    # want to ensure that you're using your own additions.
+    attr_reader :lex_filenum
+
+    # Get the list of words (and their frequencies within the WordNet graph) contained
+    # in this Synset.
+    attr_reader :word_counts
+
+    # Get the part of speech type of this synset. One of 'n' (noun), 'v' (verb), 'a' (adjective), or 'r' (adverb)
+    attr_reader :synset_type
+
+    # Get the offset, in bytes, at which this synset's POS information is stored in WordNet's internal DB.
+    # You almost certainly don't care about this.
+    attr_reader :pos_offset
+
+    # Get a shorthand representation of the part of speech this synset represents, e.g. "v" for verbs.
+    attr_reader :pos
+
+    # Get a string representation of this synset's gloss. "Gloss" is a human-readable
+    # description of this concept, often with example usage, e.g:
+    #
+    #    move upward; "The fog lifted"; "The smoke arose from the forest fire"; "The mist uprose from the meadows"
+    #
+    # for the second sense of the verb "fall"
+    attr_reader :gloss
 
     # Create a new synset by reading from the data file specified by +pos+, at +offset+ bytes into the file. This is how
     # the WordNet database is organized. You shouldn't be creating Synsets directly; instead, use Lemma#synsets.
@@ -54,7 +83,7 @@ module WordNet
     # Synset. Mostly, this is an internal method used by convience methods (e.g. Synset#antonym), but
     # it can take any valid valid +pointer_symbol+ defined in pointers.rb.
     #
-    # Example (get the gloss of an antonym for 'fall'): 
+    # Example (get the gloss of an antonym for 'fall'):
     #     WordNet::Lemma.find("fall", :verb).synsets[1].relation("!")[0].gloss
     def relation(pointer_symbol)
       @pointers.select { |pointer| pointer.symbol == pointer_symbol }.
