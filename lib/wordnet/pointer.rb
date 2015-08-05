c12b325c62a0d5c7789c29197eb2b52c19e867b5
Trevor Fountain <doches@gmail.com>
2015-08-05 12:49:21 +0100

    Inline documentation for pointer getters

--- lib/wordnet/pointer.rb
+++ lib/wordnet/pointer.rb
@@ -1,6 +1,20 @@
 module WordNet
   class Pointer
-    attr_reader :symbol, :offset, :pos, :source, :target
+    # The symbol that devices the relationship this pointer represents, e.g. "!" for verb antonym. Valid
+    # pointer symbols are defined in pointers.rb
+    attr_reader :symbol
+
+    # The offset, in bytes, of this pointer in WordNet's internal database.
+    attr_reader :offset
+
+    # The part of speech this pointer represents. One of 'n', 'v', 'a' (adjective), or 'r' (adverb).
+    attr_reader :pos
+
+    # The synset from which this pointer...points.
+    attr_reader :source
+
+    # The synset to which this pointer...points.
+    attr_reader :target
 
     # Create a pointer. Pointers represent the relations between the words in one synset and another,
     # and are referenced by a shorthand symbol (e.g. '!' for verb antonym). The list
